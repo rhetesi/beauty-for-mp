2fc935c27cb17fe683c32c1481e1fde9a3f38f89
rhetesi <rhetesi@gmail.com>
2022-02-22 23:03:06 +0100

    Deleted the unnecessary things (files, styles, comments, etc.) && add some comment to the main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,18 @@ window.onscroll = function () {
 };
 
 function changeOpacity() {
-    let opacity = 1;
-    let scrolled = document.documentElement.scrollTop;
+    let opacity = 1; // set opacity to less opaque;
+    let scrolled = document.documentElement.scrollTop; // set scrolled to how many pixel is scrolled
+    // change the value of opacity variable by the scrolled value
     if (scrolled <= 99) {
         opacity = 1 - (scrolled / 100);
     } else {
         opacity = 0.01
     }
     // console.log(`scrolled: ${scrolled}, opacity: ${opacity}`);
-    navbar.style.background = `rgba(10,10,125,${opacity})`;
+    navbar.style.background = `rgba(10,10,125,${opacity})`; // change the background opacity of the selected HTML element
+
+    // toggle navber-dark and navbar-light theme
     if (scrolled >= 50) {
         navbar.classList.remove('navbar-dark');
         navbar.classList.add('navbar-light')
