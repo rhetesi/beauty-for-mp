07e5852c9d0c29221722c23bd92cbc356034a689
rhetesi <rhetesi@gmail.com>
2022-02-22 22:49:46 +0100

    Change navbar opacity function works :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const navbar = document.querySelector('.navbar'); // Select the HTML element
+const navbarbgcolor = document.querySelector('.bgdvnav'); // Select the CSS property to change in the HTML
+
+window.onscroll = function () {
+    changeOpacity()
+};
+
+function changeOpacity() {
+    let opacity = 1;
+    let scrolled = document.documentElement.scrollTop;
+    if (scrolled <= 99) {
+        opacity = 1 - (scrolled / 100);
+    } else {
+        opacity = 0.01
+    }
+    // console.log(`scrolled: ${scrolled}, opacity: ${opacity}`);
+    navbar.style.background = `rgba(10,10,125,${opacity})`;
+    if (scrolled >= 50) {
+        navbar.classList.remove('navbar-dark');
+        navbar.classList.add('navbar-light')
+    } else {
+        navbar.classList.remove('navbar-light');
+        navbar.classList.add('navbar-dark')
+    }
+    // console.log(navbar.classList);
+}
